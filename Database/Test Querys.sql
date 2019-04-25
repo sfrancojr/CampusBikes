599376bfb9557cfdf48f4379b61415b5fc24b0b9
sfrancojr <49999758+sfrancojr@users.noreply.github.com>
2019-04-25 17:22:17 -0500

    Add files via upload

---
+++ Database/Test Querys.sql
@@ -0,0 +1,4 @@
+Select CustomerID, EmployeeID from bikeshop.onlinetracker;
+Select CustomerID, FirstName, LastName, State from bikeshop.customer;
+Select EmployeeID, FirstName, LastName, Email from bikeshop.repairteam;
+Select * from bikeshop.ticket; 
---
+++ Database/customer database.sql
@@ -0,0 +1,1 @@
+SELECT * FROM bikeshop.customer;
