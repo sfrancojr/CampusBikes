6f304b9d6591b04d0d1360faffcf2014162508bf
sfrancojr <49999758+sfrancojr@users.noreply.github.com>
2019-05-02 17:34:29 -0500

    Delete Test Querys.sql

--- Database/Test Querys.sql
+++
@@ -1,4 +0,0 @@
-Select CustomerID, EmployeeID from bikeshop.onlinetracker;
-Select CustomerID, FirstName, LastName, State from bikeshop.customer;
-Select EmployeeID, FirstName, LastName, Email from bikeshop.repairteam;
-Select * from bikeshop.ticket; 
